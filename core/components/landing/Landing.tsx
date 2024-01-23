07d75a5a1169185e60b4fca7d84af1a7b92e25c1
jrsmth-tier2 <jsmith@tier2consulting.com>
2024-01-23 15:50:14 +0000

    [CARD-23] Add landing page to git

---
+++ core/components/landing/Landing.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Landing.module.scss';
+
+const Landing = () => {
+    return (
+        <section id={styles.landingSection}>
+            <div id={styles.backgroundImage} className={styles.overlay}></div>
+            <div id={styles.orangeOverlay} className={styles.overlay}></div>
+            <div id={styles.yellowOverlay} className={styles.overlay}></div>
+            <div id={styles.darkOverlay1} className={styles.overlay}></div>
+            <div id={styles.darkOverlay2} className={styles.overlay}></div>
+        </section>
+    )
+}
+
+export default Landing;
